fc2cdc88cb7915b8bc9396c358f2a72afce87048
Anwesh43 <anweshthecool0@gmail.com>
2019-10-28 10:08:31 +0530

    adding JumpBoxTaker class

--- jumpboxtakerview/src/main/java/com/anwesh/uiprojects/jumpboxtakerview/JumpBoxTakerView.kt
+++ jumpboxtakerview/src/main/java/com/anwesh/uiprojects/jumpboxtakerview/JumpBoxTakerView.kt
@@ -154,4 +154,28 @@ class JumpBoxTakerView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class JumpBoxTaker(var i : Int) {
+
+        private val root : JBTNode = JBTNode(0)
+        private var curr : JBTNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
