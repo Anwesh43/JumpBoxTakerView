9ba4d6c9b0175075dba87c2f75f94ad0f7b1e29a
Anwesh43 <anweshthecool0@gmail.com>
2019-10-28 10:15:07 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedjumpboxtakerview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedjumpboxtakerview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedjumpboxtakerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.jumpboxtakerview.JumpBoxTakerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        JumpBoxTakerView.create(this)
     }
 }
