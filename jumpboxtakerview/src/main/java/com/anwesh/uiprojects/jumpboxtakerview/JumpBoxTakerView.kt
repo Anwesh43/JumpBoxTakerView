07583e87f34e199a2c638319b90bb9f3c75bcf8b
Anwesh43 <anweshthecool0@gmail.com>
2019-10-28 09:46:36 +0530

    adding constants and libraries

--- jumpboxtakerview/src/main/java/com/anwesh/uiprojects/jumpboxtakerview/JumpBoxTakerView.kt
+++ jumpboxtakerview/src/main/java/com/anwesh/uiprojects/jumpboxtakerview/JumpBoxTakerView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.jumpboxtakerview
 /**
  * Created by anweshmishra on 28/10/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#0D47A1")
+val backColor : Int = Color.parseColor("#BDBDBD")
